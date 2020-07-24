f3a95959e97114c0a724201d9181113647584c54
Jurate <dev.jurate@gmail.com>
2020-07-24 09:28:57 +0100

    compiled js and css; amended tailwing configuration (line heights)

--- tailwind.config.js
+++ tailwind.config.js
@@ -331,9 +331,9 @@ module.exports = {
       none: '1',
       tight: '1.25',
       snug: '1.375',
-      normal: '32px',
-      relaxed: '40px',
-      loose: '48px',
+      normal: '1.5',
+      relaxed: '1.625',
+      loose: '2',
     },
     listStyleType: {
       none: 'none',
