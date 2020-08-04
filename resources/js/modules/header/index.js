7113aba555b686e121edb8ecaf1c4bcd81064475
Jurate <dev.jurate@gmail.com>
2020-08-04 11:26:46 +0100

    increased shadow for header

--- resources/js/modules/header/index.js
+++ resources/js/modules/header/index.js
@@ -3,9 +3,9 @@ import $$ from '@utilities/selectors';
 const headerScroll = function headerScroll() {
   window.addEventListener('scroll', function() {
     if ( window.scrollY > 0 ) {
-      $$.header.classList.add('bg-white', 'shadow');
+      $$.header.classList.add('bg-white', 'shadow-md');
     } else {
-      $$.header.classList.remove('bg-white', 'shadow');
+      $$.header.classList.remove('bg-white', 'shadow-md');
     }
   });
 }();
