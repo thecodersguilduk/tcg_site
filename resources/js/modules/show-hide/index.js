e37a2fa29b9b82ae9f7cf1d748fa69b59dd9d11a
Jurate <dev.jurate@gmail.com>
2020-08-05 15:10:25 +0100

    functionality for toggle show hide (using for footer)

--- resources/js/modules/show-hide/index.js
+++ resources/js/modules/show-hide/index.js
@@ -1,32 +1,49 @@
-// import $$ from '@utilities/selectors';
+import $$ from '@utilities/selectors';
 
-// const ShowHide = function ShowHide() {
-//   let content,
-//       chevron;
+function loopThroughChildren(parent, className) {
+  if ( !parent || !className || !parent.childNodes.length) {
+    console.log("Please check if all correct arguments are provided for loopThroughChildren() function!");
+    return;
+  }
+  for (let i = 0; i < parent.childNodes.length; i++ ) {
+    if (parent.childNodes[i].classList.contains(className)) {
+      return parent.childNodes[i];
+    }
+  }
+}
 
-//   $$.toggleShowHide.forEach(btn => {
-//     btn.addEventListener('click', function(e) {
-//       content = this.nextElementSibling;
-//       content.maxHeight = content.scrollHeight;
+const ShowHide = function ShowHide() {
+  let parentNode,
+      content,
+      iconUp,
+      iconDown;
 
-//       for ( let i = 0; i < this.childNodes.length; i++ ) {
-//         if (this.childNodes[i].classList.contains('chevron')) {
-//           chevron = this.childNodes[i];
-//         }
-//       }
+  $$.toggleShowHide.forEach(btn => {
+    btn.addEventListener('click', function(e) {
+      parentNode = this.parentNode;
+
+      content = loopThroughChildren(parentNode, 'footer-dropdown-content');
+
+      if (!content) return;
+
+      iconDown = loopThroughChildren(this, 'fa-angle-down');
+      iconUp = loopThroughChildren(this, 'fa-angle-up');
+
+      content.maxHeight = content.scrollHeight;
   
-//       if (!content.style.maxHeight) {
-//         chevron.classList.remove('fa-chevron-down');
-//         chevron.classList.add('fa-chevron-up');
-//         content.style.maxHeight = `${content.maxHeight}px`;
-//       } else {
-//         chevron.classList.add('fa-chevron-down');
-//         chevron.classList.remove('fa-chevron-up');
-//         content.style.maxHeight = null;
-//       }
+      if (!content.style.maxHeight) {
+        iconDown.classList.remove('fa-angle-down');
+        iconDown.classList.add('fa-angle-up');
+        content.classList.remove('opacity-0');
+        content.style.maxHeight = `${content.maxHeight}px`;
+      } else {
+        iconUp.classList.add('fa-angle-down');
+        iconUp.classList.remove('fa-angle-up');
+        content.classList.add('opacity-0');
+        content.style.maxHeight = null;
+      }
+    });
+  });
+}();
 
-//     });
-//   });
-// }();
-
-// export default ShowHide
+export default ShowHide
