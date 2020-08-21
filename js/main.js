3d5c25e0e52f93adc4a726f9a98fc20753413e9e
Jurate <dev.jurate@gmail.com>
2020-08-21 14:32:41 +0100

    Contact Us page: spacing and font sizes amends (desktop)

--- js/main.js
+++ js/main.js
@@ -348,7 +348,7 @@ var $$ = {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nSyntaxError\n\n(132:3) `@apply` cannot be used with `.text-default` because `.text-default` either cannot be found, or its actual definition includes a pseudo-selector like :hover, :active, etc. If you're sure that `.text-default` exists, make sure that any `@import` statements are being properly processed *before* Tailwind CSS sees your CSS, as `@apply` can only be used for classes in the same CSS tree.\n\n \u001b[90m 130 | \u001b[39m\n \u001b[90m 131 | \u001b[39minput \u001b[33m{\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 132 | \u001b[39m  \u001b[36m@apply\u001b[39m text-default\u001b[33m;\u001b[39m\n \u001b[90m     | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 133 | \u001b[39m\u001b[33m}\u001b[39m\n \u001b[90m 134 | \u001b[39m\n\n    at runLoaders (/Users/jurate/Desktop/tcg_site/node_modules/webpack/lib/NormalModule.js:316:20)\n    at /Users/jurate/Desktop/tcg_site/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/jurate/Desktop/tcg_site/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/Users/jurate/Desktop/tcg_site/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at Promise.resolve.then.then.catch (/Users/jurate/Desktop/tcg_site/node_modules/postcss-loader/src/index.js:207:9)");
+// removed by extract-text-webpack-plugin
 
 /***/ }),
 
