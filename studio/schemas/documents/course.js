a29af1ec722239e71fca1124e074a99ba893874f
Steve Marks <stevenmarks75@gmail.com>
2021-09-13 12:50:52 +0100

    change data source on apprenticeship component to sanity

--- studio/schemas/documents/course.js
+++ studio/schemas/documents/course.js
@@ -7,6 +7,11 @@ export default {
       ],
     fields: [
       {
+        name: 'isActive',
+        type: 'boolean',
+        title: 'Active'
+      },
+      {
         name: 'title',
         type: 'string',
         title: 'Title',
