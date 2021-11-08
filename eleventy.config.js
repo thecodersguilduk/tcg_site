24cc247824a3a76ffee0e05de2be3a5dbf8dccf6
Steve Marks <stevenmarks75@gmail.com>
2021-11-08 15:34:29 +0000

    add image shortcode

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,50 @@
 const htmlmin = require("html-minifier")
+const Image = require('@11ty/eleventy-img');
+const path = require('path');
+
+async function imageShortcode(src, alt) {
+    let sizes = "(min-width: 1024px) 100vw, 50vw"
+    // let srcPrefix = `./src/img/`
+    // src = srcPrefix + src
+    //console.log(`Generating image(s) from:  ${src}`)
+    if(alt === undefined) {
+      // Throw an error on missing alt (alt="" works okay)
+      throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`)
+    }
+    let metadata = await Image(src, {
+      widths: [400, 600, 900, 1500],
+      formats: ['webp', 'jpeg'],
+      urlPath: "./img/",
+      outputDir: "./dist/images/",
+      /* =====
+      Now we'll make sure each resulting file's name will
+      make sense to you. **This** is why you need
+      that `path` statement mentioned earlier.
+      ===== */
+      filenameFormat: function (id, src, width, format, options) {
+        const extension = path.extname(src)
+        const name = path.basename(src, extension)
+        return `${name}-${width}w.${format}`
+      }
+    })
+    let lowsrc = metadata.jpeg[0]
+    //console.log(lowsrc);
+    let highsrc = metadata.jpeg[metadata.jpeg.length - 1]
+    return `<picture>
+      ${Object.values(metadata).map(imageFormat => {
+        return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`
+      }).join("\n")}
+      <img
+        src="${lowsrc.url}"
+        alt="${alt}"
+        loading="lazy"
+        decoding="async">
+    </picture>`
+  }
 
 module.exports = eleventyConfig => {
+  //add image shortcode
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
 
   // Add a readable date formatter filter to Nunjucks
   eleventyConfig.addFilter("dateDisplay", require("./filters/dates.js"))
