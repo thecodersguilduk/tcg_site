5d09716d09826511822e66a383df9c0581d9db22
Craig <craigclement@hotmail.com>
2021-11-24 15:50:06 +0000

    master merge

--- studio/schemas/documents/components/callModal.js
+++ studio/schemas/documents/components/callModal.js
@@ -1,16 +1,16 @@
 export default {
-    name: 'callModal',
-    title: 'Modal - Book a call',
-    type: 'object',
-    initialValue: {
-        title: 'Book a Call',
-    },
-    fields: [
-        {
-            name: 'title',
-            type: 'string',
-            title: 'Title',
-            description: 'Default value is Book a call'
-        }
-    ]
+  name: 'callModal',
+  title: 'Modal - Book a call',
+  type: 'object',
+  initialValue: {
+    title: 'Book a Call',
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title',
+      description: 'Default value is Book a call'
+    }
+  ]
 }
