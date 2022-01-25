eb0f52295978fe435cedc7c427d80c02eb96ec7f
Craig <craigclement@hotmail.com>
2022-01-25 10:25:36 +0000

    craig node installand config update

--- site/globals/config.js
+++ site/globals/config.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 
 const config = {
-    projectId: process.env.PROJECTID,
-    dataset: process.env.DATASET,
-    apiVersion: process.env.API_VERSION, // use current UTC date - see "specifying API version"!
-    token: process.env.TOKEN,
-    useCdn: process.env.USE_CDN
-      
+  projectId: 'wd1bon7z',
+  dataset: 'production',
+  apiVersion: '2021-06-07',
+  token: 'sk5wgUiW1yj5HqoLWUNWucS0DuWdacfPBw83aFoFaAGJFnQL6wDRlSCJ5Xg1Nua5EHPqZ0UjC5N6gMmzKrYyXE9DbEFzJWagHQ20oSYclK9AxsjcmwbkzzzEWpJrvSO10xEevDS0AULCa9lfz8u22NM18R3sh0R84aTWCNq36kq1f5Pt8jra',
+  useCdn: false
+
 };
 
 module.exports = config
