5bdc1efdacce7fa4e683ea3b8a7b4d33728b9958
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2023-05-19 12:38:06 +0100

    add course instances to courses

--- site/globals/courses.js
+++ site/globals/courses.js
@@ -120,22 +120,36 @@ function prepPost(data) {
     })
   }
 
+  if (data.instances) {
+    data.instances = data.instances.map((instance) => {
+      if (instance.description) {
+        instance.description = blocksToHtml({
+          blocks: instance.description,
+          serializers: serializers,
+        });
+      }
+      return instance;
+    });
+  }
+
+  console.log(data.title, data.instances);
+
   data.courseType = data.courseType ? data.courseType[0].courseType : 'Get Ahead'
 
   data.courseItemImage = data.featuredImage ? urlFor(data.featuredImage).width(500).url() : 'https://images.unsplash.com/photo-1517430816045-df4b7de11d1d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29tcHV0ZXJzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
 
   data.featuredImage = data.featuredImage ? urlFor(data.featuredImage).width(530).height(353).url() : 'https://images.unsplash.com/photo-1517430816045-df4b7de11d1d?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29tcHV0ZXJzfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60'
 
-  data.start = data.start || 'TBC'
+  data.start = data.instances ? data.instances[0].date : 'TBC';
 
   if(data.duration){
-    data.duration = data.duration.map(item => item.name)
+    data.duration = data.duration.map(item => item.name);
   } else {
-    data.duration = 'Ongoing'
+    data.duration = 'Ongoing';
   }
 
   if(data.location){
-    data.location = data.location[0]
+    data.location = data.location[0];
   }
 
   if(data.testimonials){
@@ -152,9 +166,6 @@ function prepPost(data) {
   if(data.title === 'Skills Bootcamp in Web Design and Development'){
     console.log(data);
   }
-
-
-
   return data
 }
 
