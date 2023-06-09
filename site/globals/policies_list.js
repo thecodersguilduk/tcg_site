a4aefd95f9661c082ef91bcee4e19105f01c2576
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2023-06-09 11:03:30 +0100

    change policies to come from monday board through api

--- site/globals/policies_list.js
+++ site/globals/policies_list.js
@@ -1,27 +1,70 @@
-const sanityClient = require('@sanity/client')
-const config = require('../globals/config');
+require("dotenv").config({ path: "../../.env" });
+const axios = require("axios");
 
-const query = `*[_type == "policies" && !(_id in path("drafts.**"))] {
-    ...
+const MONDAY_API_KEY = process.env.MONDAY_API_KEY;
+const BOARD_ID = process.env.POLICIESBOARD_ID;
 
-}`
+const policies = async () => {
+  try {
+    const url = `https://api.monday.com/v2/`;
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: MONDAY_API_KEY,
+    };
 
-module.exports = async function () {
-  // Fetches data
-  const client = sanityClient(config)
-  const data = await client.fetch(query)
+    const query = `query {
+                boards(ids: ${BOARD_ID}) {
+                  items{
+                    name
+                    column_values(ids : ["checkbox", "file"]){
+                      id
+                      value
+                    }
+                  }
+                }
+              }
+            `;
 
-  // Modifies the data to fit our needs
-  const preppedData = data.map(prepPost)
+    const response = await axios.post(url, { query }, { headers });
 
-  // returns this to the 11ty data cascade
-  return preppedData
-}
+    const items = response.data.data.boards[0].items;
+    const filteredItems = items.filter((item) => {
+      const checkboxValue = item.column_values.find(
+        (column) => column.id === "checkbox"
+      );
 
+      if (checkboxValue.value) {
+        const checkboxData = JSON.parse(checkboxValue.value);
+        return checkboxData.checked === "true";
+      }
 
-// This is mostly Sanity specific, but is a good function idea for preparing data
-function prepPost(data) {
+      return false;
+    });
 
-  return data
-}
+    const mappedItems = filteredItems.map((item) => {
+      const columnValues = item.column_values;
+      let fileLink;
 
+      if (columnValues) {
+        const parsedColumnValues = JSON.parse(columnValues[0].value);
+        fileLink = parsedColumnValues.files[0].linkToFile;
+      } else {
+        fileLink = "";
+      }
+
+      return {
+        name: item.name,
+        link: fileLink,
+      };
+    });
+
+    return mappedItems;
+  } catch (error) {
+    console.error("Error fetching data from Monday.com API:", error);
+    return error;
+  }
+};
+
+console.log(policies());
+
+module.exports = policies;
