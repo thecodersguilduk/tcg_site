61a7c75fbe2f8d5bcee472f76a6834e1cd441ff6
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2023-08-11 12:01:23 +0100

    add serverless function to contact form for testing

---
+++ netlify/functions/contactForm.js
@@ -0,0 +1,21 @@
+// netlify/functions/submitForm.js
+exports.handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+    return {
+      statusCode: 405,
+      body: JSON.stringify({ error: "Method not allowed" }),
+    };
+  }
+
+  const formData = JSON.parse(event.body);
+
+  // You can process the form data here (send emails, store in database, etc.)
+
+  return {
+    statusCode: 302, // Redirect
+    headers: {
+      Location: "/thanks-contact", // Redirect to the thank you page
+    },
+    body: JSON.stringify({ message: "Form submitted successfully" }),
+  };
+};
