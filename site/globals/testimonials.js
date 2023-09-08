2a16c5a2ec903e0404af8c07a90f4bc922d0580f
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2023-09-08 15:51:08 +0100

    add ldcf23 page

--- site/globals/testimonials.js
+++ site/globals/testimonials.js
@@ -1,4 +1,4 @@
-const sanityClient = require('@sanity/client')
+const sanityClient = require('@sanity/client');
 const imageUrlBuilder = require('@sanity/image-url');
 const query = `*[_type == "testimonial" && !(_id in path("drafts.**"))] {
    "avatar": avatar.asset->url,
@@ -7,33 +7,30 @@ const query = `*[_type == "testimonial" && !(_id in path("drafts.**"))] {
    },
     ...
 
-  } | order(order asc)`
+  } | order(order asc)`;
 const config = require('../globals/config');
 
-module.exports = async function() {
-    // Fetches data
-    const client = sanityClient(config)
-    const data = await client.fetch(query)
+module.exports = async function () {
+	// Fetches data
+	const client = sanityClient(config);
+	const data = await client.fetch(query);
 
-    const preppedData = data.map(prepData)
-    return preppedData
-}
+	const preppedData = data.map(prepData);
+	return preppedData;
+};
 
 function prepData(data) {
-    // Returns back to our main function
-    data.avatar = data.avatar ? urlFor(data.avatar) : ''
+	// Returns back to our main function
+	data.avatar = data.avatar ? urlFor(data.avatar) : '';
 
-    if(data.tag){
-      data.tag = data.tag.map(item => item.title);
-    }
+	if (data.tag) {
+		data.tag = data.tag.map((item) => item.title);
+	}
 
-    //console.log(data);
-    return data
+	return data;
 }
 
-
 function urlFor(source) {
-    const imageBuilder = imageUrlBuilder(sanityClient(config));
-    return imageBuilder.image(source);
-  }
-
+	const imageBuilder = imageUrlBuilder(sanityClient(config));
+	return imageBuilder.image(source);
+}
