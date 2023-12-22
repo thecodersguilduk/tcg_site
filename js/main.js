ff6a1fb6d5dff871a5c3bc21e63f45c3574739d0
Steve Marks <stevenmarks75@gmail.com>
2023-12-22 15:08:29 +0000

    remove indigo on hover

--- js/main.js
+++ js/main.js
@@ -1694,7 +1694,7 @@ var $$ = {
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// removed by extract-text-webpack-plugin
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/postcss-loader/src/index.js):\nSyntaxError\n\n(4:3) The `w-full` class does not exist. If `w-full` is a custom class, make sure it is defined within a `@layer` directive.\n\n \u001b[90m 2 | \u001b[39m\u001b[36m@tailwind\u001b[39m base\u001b[33m;\u001b[39m\n \u001b[90m 3 | \u001b[39m\u001b[33m[\u001b[39mdata-lazy\u001b[33m]\u001b[39m \u001b[33m{\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 4 | \u001b[39m  \u001b[36m@apply\u001b[39m opacity-0 w-full\u001b[33m;\u001b[39m\n \u001b[90m   | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 5 | \u001b[39m  transition\u001b[33m:\u001b[39m opacity 0.25s ease-in\u001b[33m;\u001b[39m\n \u001b[90m 6 | \u001b[39m  min-height\u001b[33m:\u001b[39m 120px\u001b[33m;\u001b[39m\n\n    at /home/steve/tcg_site/node_modules/webpack/lib/NormalModule.js:316:20\n    at /home/steve/tcg_site/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /home/steve/tcg_site/node_modules/loader-runner/lib/LoaderRunner.js:233:18\n    at context.callback (/home/steve/tcg_site/node_modules/loader-runner/lib/LoaderRunner.js:111:13)\n    at /home/steve/tcg_site/node_modules/postcss-loader/src/index.js:207:9");
 
 /***/ }),
 
