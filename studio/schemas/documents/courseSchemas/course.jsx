ded1e77d3ef345345b6b63caaae1c2b45f5b435d
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-04-15 12:07:13 +0100

    change eoi forms back

--- studio/schemas/documents/courseSchemas/course.jsx
+++ studio/schemas/documents/courseSchemas/course.jsx
@@ -99,6 +99,13 @@ export default {
 			description: 'This controls the text that appears in the CTA buttons on the course page. Defaults to apply now.'
 		},
 		{
+			name: 'formLink',
+			title: 'Form Link',
+			description: 'Link to the course application form',
+			type: 'string',
+			fieldset: 'metaHero',
+		},
+		{
 			name: 'meta_description',
 			title: 'Meta Description',
 			description: 'For google and perhaps rendering on the page!',
@@ -332,12 +339,6 @@ export default {
 				source: 'title',
 			},
 			maxLength: 96,
-		},
-		{
-			name: 'formLink',
-			title: 'Form Link',
-			description: 'Link to the course application form - default will be the google form for Short Courses',
-			type: 'string'
 		},
 		{
 			name: 'tags',
