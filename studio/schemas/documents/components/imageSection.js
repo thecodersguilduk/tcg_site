80a6dceb4baf671be44fad9eb3e38a56be5d8d96
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-06-05 11:19:14 +0100

    add new fields to sanity

--- studio/schemas/documents/components/imageSection.js
+++ studio/schemas/documents/components/imageSection.js
@@ -1,5 +1,19 @@
 export default {
   name: 'imageSection',
   title: 'Image',
-  type: 'image'
+  type: 'image',
+  fields: [
+    {
+      name: 'alt',
+      type:'string',
+      title: 'Alt text',
+      description: 'This text will be read by screen readers and should describe the image'
+    },
+    {
+      name: 'license',
+      type:'string',
+      title: 'License link',
+      description: 'This text will the link for the CC license'
+    }
+  ]
 }
