a72186f95130dc9308b0bb03883be3c8df11358e
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2024-10-16 14:49:13 +0100

    second draft

--- resources/js/modules/slider/index.js
+++ resources/js/modules/slider/index.js
@@ -1,8 +1,9 @@
 const sliderSettings = function(){
   const reviewsSlick = document.querySelector('.testimonials__slider');
   const courseSlick = document.querySelector('.course__testimonials__slider');
+  const consultantSlider = document.querySelector('.consultant__slider');
 
-  if(!reviewsSlick && !courseSlick) return
+  if(!reviewsSlick && !courseSlick && !consultantSlider) return
 
   if(reviewsSlick){
     return $('.testimonials__slider').slick({
@@ -22,6 +23,18 @@ const sliderSettings = function(){
       prevArrow: null,
       nextArrow: null,
       appendDots: $('.dots'),
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    });
+  }
+
+  if(consultantSlider){
+    return $('.consultant__slider').slick({
+      dots: true,
+      prevArrow: $('.consultant__slider + div .prev'),
+      nextArrow: $('.consultant__slider + div .next'),
+      appendDots: $('.consultant__slider + div .dots'),
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
