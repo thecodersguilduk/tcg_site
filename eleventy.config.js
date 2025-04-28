0bce88bebb2307ef3bbdd6ba1b6a7d9a2f774cb7
Steve Marks <42617540+Stig11686@users.noreply.github.com>
2025-04-28 15:26:58 +0100

    fix blog issue

--- eleventy.config.js
+++ eleventy.config.js
@@ -74,9 +74,6 @@ module.exports = (eleventyConfig) => {
 	// Collections
 	eleventyConfig.addCollection('blog', (collection) => {
 		let blogs = collection.getFilteredByTag('blog');
-		let caseStudies = collection.getFilteredByTag('Case Studies');
-
-		blogs = blogs.concat(caseStudies);
 
 		for (let i = 0; i < blogs.length; i++) {
 			const prevPost = blogs[i - 1];
